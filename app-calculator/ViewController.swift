b9a9e43fdcc8b4882ef751a8a63259233b8430c6
Andrei Dumitrescu <mordre@Mordres-Mac.local>
2017-10-13 05:38:29 -0700

    Added Auto Layout

--- app-calculator/ViewController.swift
+++ app-calculator/ViewController.swift
@@ -14,6 +14,25 @@ class ViewController: UIViewController {
     
     var userIsTyping = false
     
+    private func showSizeClasses() {
+        if !userIsTyping {
+            display.textAlignment = .center
+            display.text = "width " + traitCollection.horizontalSizeClass.description + " height" + traitCollection.verticalSizeClass.description
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showSizeClasses()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: { coordinator in
+            self.showSizeClasses()
+        }, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         brain.addUnaryOperation(named: "✅") { [weak weakSelf = self] in
@@ -57,4 +76,15 @@ class ViewController: UIViewController {
         }
     }
 }
-
+extension UIUserInterfaceSizeClass: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .compact:
+            return "Compact"
+        case .regular:
+            return "Regular"
+        case .unspecified:
+            return "??"
+        }
+    }
+}
